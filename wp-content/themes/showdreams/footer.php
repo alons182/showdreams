9c4c30ee5369eba7212bf8d8d010015191712bda
Alonso Chavarria <alons182@hotmail.com>
2015-05-12 10:31:46 -0600

    se agrego fb page

--- wp-content/themes/showdreams/footer.php
+++ wp-content/themes/showdreams/footer.php
@@ -3,11 +3,11 @@
                 <div class="footer__contact">
                     <a href="<?php echo home_url( '/' ); ?>" class="footer__logo"><img src="<?php echo get_template_directory_uri(); ?>/img/logo-white.png" alt="Show Dreams" class="footer__logo__img" /></a>
                     <div class="footer__contact__item footer__contact__item--tel">
-                         8480-68-65 <br/>
+                         (506) 8480-68-65 <br/>
                         <span>Lorem ipsum dolor sit amet</span>
                     </div>
                     <div class="footer__contact__item footer__contact__item--tel">
-                        8824-37-03 <br/>
+                        (506) 8824-37-03 <br/>
                         <span>Lorem ipsum dolor sit amet</span>
                     </div>
                     <div class="footer__contact__item footer__contact__item--mail">
@@ -27,7 +27,8 @@
                              )
 
                         ); ?>
-                    
+                    <div class="fb-page" data-href="https://www.facebook.com/pages/Show-Dreams/333030813484914" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/pages/Show-Dreams/333030813484914"><a href="https://www.facebook.com/pages/Show-Dreams/333030813484914">Show Dreams</a></blockquote></div>
+                    </div>
                     <div class="footer__social">
                         <h4>Siguenos en:</h4>
                         <div class="footer__social__item">
@@ -65,6 +66,16 @@
         
 
         <script src="<?php echo get_template_directory_uri() ?>/js/bundle.js"></script>
+        
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.3&appId=363306470411928";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+
         <?php wp_footer(); ?>
     </body>
 </html>
