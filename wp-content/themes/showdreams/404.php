b4efe3c7119923a651f5d66f32c261f50e664d14
Alonso Chavarria Cubero <alons182@hotmail.com>
2016-12-06 14:49:54 -0600

    404-2

--- wp-content/themes/showdreams/404.php
+++ wp-content/themes/showdreams/404.php
@@ -17,7 +17,7 @@ get_header(); ?>
                 
 
                             <a href="<?php echo home_url( '/contact-us' ); ?>" class="btn"><span><?php  echo (get_locale() == "es_ES") ? "Contáctenos" : "Contact Us" ?></span></a>
-                           404
+                           
                         </div>
                     </div>
             </div>
